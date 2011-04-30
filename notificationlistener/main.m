51946d66495976caea244edaa104f8e02623fa3c
Marc Liyanage <liyanage@apple.com>
2011-04-29 17:07:42 -0700

    Initial Commit

---
+++ notificationlistener/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  notificationlistener
+//
+//  Created by Marc Liyanage on 4/29/11.
+//  Copyright 2011 Apple Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+	// insert code here...
+	NSLog(@"Hello, World!");
+
+	[pool drain];
+    return 0;
+}
+
